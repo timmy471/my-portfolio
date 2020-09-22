3ea855ab26c2c19ca5e8671d774d52258c945bf0
Godson Rapture Chijioke <coolpythoncodes@gmail.com>
2020-09-22 22:49:21 +0100

    mobile header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from './components/Header'
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div>
-      
+    <div className='container'>
+      <Header/>
     </div>
   );
 }
