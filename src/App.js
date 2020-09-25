562eca51fdcb10744a148638b3287883785dfba3
Godson Rapture Chijioke <coolpythoncodes@gmail.com>
2020-09-25 22:14:32 +0100

    bio section for mobile and desktop

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './components/Header'
 
 import './App.scss';
+import Bio from './Bio';
 
 function App() {
   return (
     <div className='container'>
       <Header/>
+      <Bio/>
     </div>
   );
 }
---
+++ src/Bio.js
@@ -0,0 +1,42 @@
+import { GitHub, Instagram, Twitter } from '@material-ui/icons';
+import React from 'react';
+
+const Bio = () => {
+    return (
+            <section className="bio">
+                <div className="bio-pic">
+                    <img src={require('./img/img.png')} />
+                </div>
+
+                <div className="bio-text">
+                    <h1>
+                        <span>Hi,</span> I'm CHIJIOKE
+                    </h1>
+                    <h2>I'm a web developer</h2>
+                    <p>
+                        Designing and telling peoples stories, through web design has been
+                        my thing for the past one year. I give my very all to each
+                        project. Don't take my word for it, click the button bellow to
+                        find out.
+                    </p>
+                    <div class="social-links">
+                        <a href="#">
+                            <GitHub/>
+                        </a>
+                        <a href="#">
+                            <Instagram/>
+                        </a>
+                        <a href="#">
+                            <Twitter/>
+                        </a>
+                    </div>
+                    <a href="#" class="hire-btn">
+                        Hire Me
+                    </a>
+                </div>
+            </section>
+
+		);
+}
+
+export default Bio;
