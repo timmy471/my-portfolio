ffb904f3b8c96e2c469be172e82eb2d8910b7124
Godson Rapture Chijioke <coolpythoncodes@gmail.com>
2020-09-25 18:26:41 +0100

    header for mobile and desktop

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,30 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <header >
-            <div className="logo">
-            <img src={require("../img/logo.png")} alt="" />
-            </div>
-            <div className="menu-btn">
-            <div className="menu-btn-line"></div>
-            <div className="menu-btn-line"></div>
-            <div className="menu-btn-line"></div>
-            </div>
-            <nav className="nav close-nav">
-            <ul className="nav-list close-nav ">
-                <li className="nav-link "><a href="#">My works</a></li>
-                <li className="nav-link "><a href="#">Contact me</a></li>
-                <li className="nav-link "><a href="#">My blog</a></li>
-            </ul>
-            </nav>
-      </header>
-    );
+			<header>
+				<div className="logo">
+					<img src={require("../img/logo.png")} alt="" />
+				</div>
+				<div className="menu-btn">
+					<div className="menu-btn-line"></div>
+					<div className="menu-btn-line"></div>
+					<div className="menu-btn-line"></div>
+				</div>
+				<nav className="nav close-nav">
+					<ul className="nav-list close-nav ">
+						<li className="nav-link ">
+							<a href="#">My works</a>
+						</li>
+						<li className="nav-link ">
+							<a href="#">Contact me</a>
+						</li>
+						<li className="nav-link ">
+							<a href="#">My blog </a>
+						</li>
+					</ul>
+				</nav>
+			</header>
+		);
 }
 
 export default Header;
