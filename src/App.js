ddc3ba376b6cabf87905d16650a227eda4e803ee
Godson Rapture Chijioke <coolpythoncodes@gmail.com>
2020-09-26 11:26:48 +0100

    work process section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import Header from './components/Header'
+import React from "react";
+import Header from "./components/Header";
 
-import './App.scss';
-import Bio from './components/Bio';
-import Project from './components/Project';
+import "./App.scss";
+import Bio from "./components/Bio";
+import Project from "./components/Project";
+import Works from "./components/Works";
 
 function App() {
-  return (
-    <div className='container'>
-      <Header/>
-      <Bio/>
-      <Project/>
-    </div>
-  );
+	return (
+		<div className="container">
+			<Header />
+			<Bio />
+			<Project />
+			<Works />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Works.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Works = () => {
+    return (
+        <section className="work-process">
+        <div className="line"></div>
+        <h1>Work Process</h1>
+        <div className="work-process-1">
+          <div className="work-circle">
+            <p>1</p>
+          </div>
+          <h3>EVALUATE REQUIREMENTS</h3>
+          <p>
+            Deep dive into your business needs and evaluate requirements for our
+            project. Develop user stories, list out and approve product features
+            and priority, develop personas.
+          </p>
+        </div>
+        <div className="work-process-2">
+          <div className="work-circle">
+            <p>2</p>
+          </div>
+          <h3>PRODUCE WIREFRAMES</h3>
+          <p>
+            Weather we need to make a website or a complicated SaaS product
+            wireframing is important stage of any UX process. Wireframes are
+            black and white layouts that outline the specific size and placement
+            of page elements, navigation for your product. They are devoclassName of
+            color, font, logos or any real design elements that take away from
+            purely focusing on a product's structure.
+          </p>
+        </div>
+        <div className="work-process-3">
+          <div className="work-circle">
+            <p>3</p>
+          </div>
+          <h3>USER INTERFACE DESIGN</h3>
+          <p>
+            I create a style to follow for the product, approve it with you and
+            dress up all the screens in it. As a result - a completely UI
+            designed product. After this I make interactive prototype to click
+            through the product even before it goes to development.
+          </p>
+        </div>
+      </section> 
+    );
+}
+
+export default Works;
