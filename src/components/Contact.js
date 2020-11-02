a79064d8fbe2241585d44e4ec264470b082a838c
Godson Rapture Chijioke <coolpythoncodes@gmail.com>
2020-11-02 14:44:14 +0100

    integrated netlify form submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,9 @@
-import { ArrowForward, LocationOn, MailOutline, Phone } from "@material-ui/icons";
+import {
+	ArrowForward,
+	LocationOn,
+	MailOutline,
+	Phone,
+} from "@material-ui/icons";
 import React from "react";
 
 export default function Contact() {
@@ -14,13 +19,25 @@ export default function Contact() {
 						<Phone /> +234 703 806 8811
 					</div>
 					<div>
-						<LocationOn /> Carlton Gate Estate, Chevron Office Drive, Lekki, Lagos
+						<LocationOn /> Carlton Gate Estate, Chevron Office Drive, Lekki,
+						Lagos
 					</div>
 				</div>
 			</div>
-            <div id="contact-message">
+			<div id="contact-message">
 				<div className="message-container">
-					<form>
+					<form
+						name="contact"
+						method="POST"
+						netlify-honeypot="bot-field"
+						data-netlify="true"
+					>
+						<p class="hidden">
+							<label>
+								Don’t fill this out if you're human: <input name="bot-field" />
+							</label>
+						</p>
+						<input type="hidden" name="form-name" value="contact" />
 						<input type="text" name="" id="" placeholder="Full Name" />
 						<input type="email" name="" id="" placeholder="Email" />
 						<textarea
@@ -31,7 +48,7 @@ export default function Contact() {
 							placeholder="Message"
 						></textarea>
 						<button type="submit">
-							<span>SEND MESSAGE</span>  <ArrowForward/>
+							<span>SEND MESSAGE</span> <ArrowForward />
 						</button>
 					</form>
 				</div>
