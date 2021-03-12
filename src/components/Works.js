7c8e306f3a70e734f49150345dfa3f1c7a77cf0c
timi471 <ayantunjitimilehin@yahoo.com>
2021-03-12 13:02:11 -0500

    gh pages

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,49 +1,48 @@
-import React from 'react';
+import React from "react";
 
 const Works = () => {
-    return (
-        <section className="work-process" >
-        <div className="line"></div>
-        <h1>Work Process</h1>
-        <div className="work-process-1">
-          <div className="work-circle">
-            <p>1</p>
-          </div>
-          <h3>EVALUATE REQUIREMENTS</h3>
-          <p>
-            Deep dive into your business needs and evaluate requirements for our
-            project. Develop user stories, list out and approve product features
-            and priority, develop personas.
-          </p>
+  return (
+    <section className="work-process">
+      <div className="line"></div>
+      <h1>Work Process</h1>
+      <div className="work-process-1">
+        <div className="work-circle">
+          <p>1</p>
         </div>
-        <div className="work-process-2">
-          <div className="work-circle">
-            <p>2</p>
-          </div>
-          <h3>PRODUCE WIREFRAMES</h3>
-          <p>
-            Weather we need to make a website or a complicated SaaS product
-            wireframing is important stage of any UX process. Wireframes are
-            black and white layouts that outline the specific size and placement
-            of page elements, navigation for your product. They are devoclassName of
-            color, font, logos or any real design elements that take away from
-            purely focusing on a product's structure.
-          </p>
+        <h3>EVALUATE REQUIREMENTS</h3>
+        <p>
+          Deep dive into your business needs and evaluate requirements for our
+          project. Discuss with business parties to list out and approve product
+          features and priority.
+        </p>
+      </div>
+      <div className="work-process-2">
+        <div className="work-circle">
+          <p>2</p>
         </div>
-        <div className="work-process-3">
-          <div className="work-circle">
-            <p>3</p>
-          </div>
-          <h3>USER INTERFACE DESIGN</h3>
-          <p>
-            I create a style to follow for the product, approve it with you and
-            dress up all the screens in it. As a result - a completely UI
-            designed product. After this I make interactive prototype to click
-            through the product even before it goes to development.
-          </p>
+        <h3>UI DESIGN AND DEVELOPMENT</h3>
+        <p>
+          Weather we need to make a website or a complicated web application,
+          putting up a design system is important to provide enough information
+          for the development phase. Collaborating with the UI/UX team in the
+          design process and wireframing to ensure uniformity in requirement and
+          results set to be achieved before actual coding and development of features
+          within the specified duration of time
+        </p>
+      </div>
+      <div className="work-process-3">
+        <div className="work-circle">
+          <p>3</p>
         </div>
-      </section> 
-    );
-}
+        <h3>TESTING AND DELIVERY</h3>
+        <p>
+          Testing implemented features while ensuring existing features, if presentt, still work
+          as expected. This is a very important phase as it validates the work put into the development
+          before deployment
+        </p>
+      </div>
+    </section>
+  );
+};
 
 export default Works;
