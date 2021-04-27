2fceea32f20e342d3f1dbf8dc07c30c44da44ca3
Timmy <timilehin.ayantunji@venturegardengroup.com>
2021-04-28 01:48:04 +0800

    Updates

--- precache-manifest.699a7d32c3638cd4217c7e8612a3ed78.js
+++ precache-manifest.699a7d32c3638cd4217c7e8612a3ed78.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "13bce36eb8c3594dee20aa9e511d8ba5",
+    "revision": "984a9e09255fb359bd2f07e302ab3988",
     "url": "/my-portfolio/index.html"
   },
   {
-    "revision": "993e477ce0d4afeaf418",
+    "revision": "17d182672432a5c53b6a",
     "url": "/my-portfolio/static/css/main.45f8f078.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my-portfolio/static/js/2.601e758d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "993e477ce0d4afeaf418",
-    "url": "/my-portfolio/static/js/main.482ca5c6.chunk.js"
+    "revision": "17d182672432a5c53b6a",
+    "url": "/my-portfolio/static/js/main.410dc41d.chunk.js"
   },
   {
     "revision": "f6b99e60d6d34b538c80",
@@ -26,6 +26,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "3d412e642c4b3d830960a32e2c80fae4",
     "url": "/my-portfolio/static/media/educollect.3d412e64.jpeg"
+  },
+  {
+    "revision": "42f291fff3584c02c9973ca344a696f1",
+    "url": "/my-portfolio/static/media/edutech.42f291ff.png"
   },
   {
     "revision": "f2717cd55bde0ac622e8ec733cc791cf",
@@ -38,10 +42,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "4569d72b270345f9962490f700493b57",
     "url": "/my-portfolio/static/media/img.4569d72b.png"
-  },
-  {
-    "revision": "af7c44cbf7987281e536fe4d03c52df6",
-    "url": "/my-portfolio/static/media/staff-mgt.af7c44cb.png"
   },
   {
     "revision": "c308bed2f1961a886c5ed33a2b7244e1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.9e723a75765ea5956c352baffc3111bc.js"
+  "/my-portfolio/precache-manifest.699a7d32c3638cd4217c7e8612a3ed78.js"
 );
 
 self.addEventListener('message', (event) => {
